b952b5a762fa4261ad25818d3911100309af5e52
Idloj <Idloj@users.noreply.github.com>
2015-09-30 21:04:59 +0300

    Make agent icons lighter when active rather than darker

--- src/main/org/nlogo/awt/ValueImageFilter.scala
+++ src/main/org/nlogo/awt/ValueImageFilter.scala
@@ -2,17 +2,24 @@
 
 package org.nlogo.awt
 
-class DarkenImageFilter(level: Double)
+class ValueImageFilter(value: Double)
 extends java.awt.image.RGBImageFilter {
+  require(value >= 0)
   private final val OpaqueAlpha = 0xff000000
   canFilterIndexColorModel = true
   override def filterRGB(x: Int, y: Int, rgb: Int): Int = {
     var red = (rgb >> 16) & 0xff
     var green = (rgb >> 8) & 0xff
     var blue = (rgb) & 0xff
-    red = ((1.0 - level) * red).toInt
-    green = ((1.0 - level) * green).toInt
-    blue = ((1.0 - level) * blue).toInt
+    red = (value * red).toInt
+    green = (value * green).toInt
+    blue = (value * blue).toInt
+    val difference = Math.max(red, Math.max(green, blue)) - 255
+    if (difference > 0) {
+      red -= difference
+      green -= difference
+      blue -= difference
+    }
     (rgb & OpaqueAlpha) | (red << 16) | (green << 8) | blue
   }
 }
--- src/main/org/nlogo/window/ButtonWidget.scala
+++ src/main/org/nlogo/window/ButtonWidget.scala
@@ -5,7 +5,7 @@ package org.nlogo.window
 import java.awt.{List=>AWTList, _}
 import event.{MouseEvent, MouseListener, MouseMotionListener}
 import image.FilteredImageSource
-import org.nlogo.awt.DarkenImageFilter
+import org.nlogo.awt.ValueImageFilter
 import javax.swing.ImageIcon
 import org.nlogo.util.MersenneTwisterFast
 import org.nlogo.awt.Mouse.hasButton1
@@ -18,25 +18,25 @@ object ButtonWidget {
 
   def image(path: String) = new ImageIcon(classOf[ButtonWidget].getResource(path))
 
-  val FOREVER_GRAPHIC_DARK: ImageIcon = image("/images/forever.gif")
-  val FOREVER_GRAPHIC: ImageIcon = image("/images/forever2.gif")
+  val FOREVER_ICON: ImageIcon = image("/images/forever.gif")
+  val FOREVER_ICON_ACTIVE: ImageIcon = image("/images/forever-active.gif")
 
   object ButtonType {
 
     // the 4 possible button types
-    val ObserverButton = ButtonType("observer", classOf[Observer], img = None, darkImg = None)
+    val ObserverButton = ButtonType("observer", classOf[Observer], img = None, activeImg = None)
     val TurtleButton = ButtonType("turtle", classOf[Turtle], "/images/turtle.gif")
     val LinkButton = ButtonType("link", classOf[Link], "/images/link.gif")
     val PatchButton = ButtonType("patch", classOf[Patch], "/images/patch.gif")
 
     val buttonTypes = List(ObserverButton, TurtleButton, LinkButton, PatchButton)
 
-    def darkImage(image: ImageIcon) = new ImageIcon(java.awt.Toolkit.getDefaultToolkit.createImage(
-      new FilteredImageSource(image.getImage.getSource, new DarkenImageFilter(0.5))))
+    def lightImage(image: ImageIcon) = new ImageIcon(java.awt.Toolkit.getDefaultToolkit.createImage(
+      new FilteredImageSource(image.getImage.getSource, new ValueImageFilter(1.6))))
 
     private def apply(headerCode:String, agentClass:Class[_ <: Agent], imagePath: String): ButtonType = {
       val img = image(imagePath)
-      new ButtonType(headerCode, agentClass, Some(img), Some(darkImage(img)))
+      new ButtonType(headerCode, agentClass, Some(img), Some(lightImage(img)))
     }
     def apply(c:Class[_ <: Agent]): ButtonType = {
       buttonTypes.find(_.agentClass == c).getOrElse(ObserverButton) //TODO or should we say error("bad agent class")
@@ -62,8 +62,8 @@ object ButtonWidget {
   // encapsulates what used to be a bunch of 4 way if statements.
   // ButtonWidget now has a single ButtonType object that handles all this logic for it.
   case class ButtonType(name: String, agentClass:Class[_ <: Agent],
-                        img:Option[ImageIcon], darkImg:Option[ImageIcon]){
-    def img(dark:Boolean): Option[ImageIcon] = if(dark) darkImg else img
+                        img:Option[ImageIcon], activeImg:Option[ImageIcon]){
+    def img(active: Boolean): Option[ImageIcon] = if (active) activeImg else img
     def toHeaderCode = "__" + name.toLowerCase + "code "
     def toAgentOptions = {
       val opts = ButtonType.defaultAgentOptions
@@ -374,15 +374,15 @@ class ButtonWidget(random:MersenneTwisterFast) extends JobWidget(random)
     def paintButtonRectangle(g: Graphics) {
       g.setColor(getPaintColor)
       g.fillRect(0, 0, getWidth(), getHeight())
-      def renderImages(g: Graphics, dark: Boolean) {
+      def renderImages(g: Graphics, active: Boolean) {
         def maybePaintForeverImage() {
           if (forever) {
-            val image = if (dark) FOREVER_GRAPHIC_DARK else FOREVER_GRAPHIC
+            val image = if (active) FOREVER_ICON_ACTIVE else FOREVER_ICON
             image.paintIcon(this, g, getWidth() - image.getIconWidth - 4, getHeight() - image.getIconHeight - 4)
           }
         }
         def maybePaintAgentImage() {
-          buttonType.img(dark).map(_.paintIcon(this, g, 3, 3))
+          buttonType.img(active).map(_.paintIcon(this, g, 3, 3))
         }
         maybePaintForeverImage()
         maybePaintAgentImage()
