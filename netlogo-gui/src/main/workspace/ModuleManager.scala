3f595c07fc6b3c54f1803cffca77d20e4eccb463
Idloj <Idloj@users.noreply.github.com>
2018-09-12 14:49:08 +0300

    Compile imported modules

--- netlogo-gui/src/main/workspace/ModuleManager.scala
+++ netlogo-gui/src/main/workspace/ModuleManager.scala
@@ -2,8 +2,9 @@
 
 package org.nlogo.workspace
 
-import org.nlogo.core.{ ErrorSource, FrontEndProcedure, ModuleManager => CoreModuleManager }
+import org.nlogo.core.{ ErrorSource, FrontEndProcedure, I18N, ModuleManager => CoreModuleManager, Program }
 import org.nlogo.nvm.CompilerResults
+import org.nlogo.parse.IncludeFile
 
 class ModuleManager(workspace: AbstractWorkspace) extends CoreModuleManager {
   private var modules = Map[String, CompilerResults]()
@@ -12,7 +13,22 @@ class ModuleManager(workspace: AbstractWorkspace) extends CoreModuleManager {
     modules = Map[String, CompilerResults]()
   }
 
-  override def importModule(name: String, errors: ErrorSource): Unit = {}
+  override def importModule(name: String, errors: ErrorSource): Unit = {
+    if (!modules.contains(name.toUpperCase)) {
+      IncludeFile(workspace.getCompilationEnvironment, name + ".nls") match {
+        case Some((path, source)) =>
+          modules = modules.updated(name.toUpperCase,
+            workspace.compiler.compileProgram(
+              source,
+              Program.fromDialect(workspace.dialect),
+              workspace.getExtensionManager,
+              new ModuleManager(workspace),
+              workspace.getCompilationEnvironment))
+        case None =>
+          errors.signalError(I18N.errors.getN("org.nlogo.workspace.ModuleManager.moduleNotFound", name))
+      }
+    }
+  }
 
   override def getProcedure(name: String): Option[FrontEndProcedure] = None
 }
